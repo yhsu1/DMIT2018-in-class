3d2505f38b9e670a5ad3106cb810b3182c2e9890
yhsu1 <yhsu1@studentmail.nait.ca>
2014-09-16 15:07:47 -0600

    test

---
+++ eRestaurant/eRestaurant/NewFolder2/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.NewFolder2
+{
+    public class RestaurantAdminController
+    {
+        #region Manage Waiters
+        #region Command
+        public int AddWaiter (Waiter item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWaiter(Waiter item)
+        {
+
+        }
+        #endregion
+        #region Query
+        public List<Waiter> ListAllWaiters()
+        {
+            throw new NotImplementedException();
+        }
+        public Waiter GetWaiter(int waiterId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+    }
+}
