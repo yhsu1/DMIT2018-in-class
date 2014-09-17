ee4ed65c9ac8dfa4f83d757f5ca8e5df9c9502c9
yhsu1 <yhsu1@studentmail.nait.ca>
2014-09-16 22:48:57 -0600

    test

---
+++ eRestaurant/BLL/RestaurantAdminController.cs
@@ -0,0 +1,133 @@
+﻿using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    public class RestaurantAdminController
+    {
+        #region Manage Waiters
+        #region Command
+        public int AddWaiter(Waiter item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWaiter(Waiter item)
+        {
+
+        }
+
+        public void DeleteWaiter(Waiter item) 
+        {
+        }
+        #endregion
+
+        #region Query
+        public List<Waiter> ListAllWaiters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Waiter GetWaiter(int waiterId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+
+        #region Manage Tables
+        #region Command
+        public int AddTable(Table item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTable(Table item)
+        {
+
+        }
+
+        public void DeleteTable(Table item)
+        {
+
+        }
+
+        #endregion
+        #region Query
+        public List<Table> ListAllTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Table GetTable(int tableId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+
+        #region Manage Items
+        #region Command
+        public int AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+
+        }
+
+        public void DeleteItem(Item item)
+        {
+
+        }
+        #endregion
+        #region Query
+        public List<Item> ListAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+
+        #region Manage Special Events
+        #region Command
+        public string AddSpecialEvent(SpecialEvent item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(SpecialEvent item)
+        {
+
+        }
+
+        public void DeleteItem(SpecialEvent item)
+        {
+
+        }
+        #endregion
+        #region Query
+        public List<SpecialEvent> ListAllSpecialEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SpecialEvent GetSpecialEvents(string eventCode)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ eRestaurant/DAL/RestaurantContext.cs
@@ -0,0 +1,45 @@
+﻿using eRestaurant.Entities; // Needed for accessing my Entity classes
+using System;
+using System.Collections.Generic;
+using System.Data.Entity; // Needed for the DbContext base class
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.DAL
+{
+    public class RestaurantContext : DbContext
+    {
+        //Construction that calls a base-class constructor to specify the
+        //connection string we need to use
+        public RestaurantContext() : base("name=EatIn"){}
+
+        #region Table to Entity mappings
+        public DbSet <Table> Tables { get; set; }
+        public DbSet <SpecialEvent> SpecialEvents { get ; set; }
+        public DbSet <Reservation> Reservations { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet <Item> Items { get; set; }
+        public DbSet<MenuCategories> MenuCategories { get; set; }
+        public DbSet <Waiter> Waiters { get; set; }
+        public DbSet <BillItem> BillItems { get; set; }
+        #endregion
+
+        #region Over-ride OnModelCreating
+        // 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Reservation>().HasMany(r => r.Tables)
+                .WithMany(t => t.Reservations)
+                .Map(mapping =>
+                {
+                    mapping.ToTable("ReservationTables");
+                    mapping.MapLeftKey("ReservationID");
+                    mapping.MapRightKey("TableID");
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+    }
+}
