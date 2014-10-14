df2ee599ab9758e2a58c344a58cf87646c335762
yhsu1 <yhsu1@studentmail.nait.ca>
2014-10-14 14:19:15 -0600

    add DTOs in entities

---
+++ eRestaurant/Entities/DTOs/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+{
+    public class Order
+    {
+
+        public int TableNumber { get; set; }
+        public string Waiter { get; set; }
+        public int WaiterID { get; set; }
+        public int? BillID { get; set; }
+        public bool Served { get; set; }
+        public string OrderComments { get; set; }
+
+        public decimal TotalAmount
+        {
+            get
+            {
+                decimal value = 0; // default value
+                if (Items != null)
+                    value = Items.Sum(x => x.ItemTotal); // See footnotes
+
+                return value;
+            }
+        }
+
+        public IList<OrderItem> Items { get; set; }
+    }
+}
